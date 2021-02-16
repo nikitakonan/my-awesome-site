aea765278a17ed26b625efb887c6726a90a87ec4
nikitakonan <nikitakonan1991@gmail.com>
2021-02-16 14:19:43 +0300

    Added GTAG plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-gdpr-cookies",
+      options: {
+        googleTagManager: {
+          trackingId: process.env.TAG_MANAGER_ID,
+          cookieName: "gatsby-gdpr-google-tagmanager",
+          dataLayerName: "dataLayer",
+        },
+        environments: ["production"],
+      },
+    },
   ],
 }
